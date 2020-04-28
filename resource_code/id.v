8cca959b148f1af8942a40354b8d95d9d09ca9b0
chnjstyj <851286504@qq.com>
2020-04-28 17:14:29 +0800

    添加符号扩展

--- resource_code/id.v
+++ resource_code/id.v
@@ -4,7 +4,7 @@ module id(
     output [4:0] rreg_a,         //读寄存器1
     output [4:0] rreg_b,         //读寄存器2
     output [4:0] wreg,           //写寄存器
-    output [15:0] imme_num,       //立即数
+    output [31:0] imme_num,       //立即数
     output [5:0] func,           //指令func段 
     input RegDst                 //来自控制信号的regdis
 );
@@ -12,9 +12,9 @@ module id(
 assign opcode = inst[31:25];
 assign rreg_a = inst[25:21];
 assign rreg_b = inst[20:16];
-assign imme_num = inst[15:0];
+
 assign func = inst[5:0];
 assign wreg = (RegDst == 1'b0)?inst[20:16]:inst[15:11];
-
+assign imme_num ={16{inst[15]},inst[15:0]};       //符号扩展到32位
 
 endmodule
